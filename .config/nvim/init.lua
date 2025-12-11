ba11a58e5689af0912fa1b334a8f8a42cab53858
Shane Carmody <scarmody@tcd.ie>
2025-12-10 17:35:50 -0800

    migrate to lazy after nvim upgrade

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,9 @@
 
+-- Enable Neovim's fast Lua module loader (must be first)
+if vim.loader then
+  vim.loader.enable()
+end
+
 -- Load plugin manager
 require('user.plugins')
 
@@ -10,3 +15,11 @@ vim.cmd('syntax enable')
 vim.cmd('filetype plugin indent on')
 
 
+-- Set up plugins with dependencies
+require('user.treesitter') -- Set up before LSP for better highlighting
+-- require('user.lsp')  -- Depends on language servers being available
+require('user.completion') -- Depends on LSP configuration
+require('user.telescope') -- Often integrates with LSP
+
+-- Configure UI components last
+require('user.theme')
--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -1,27 +1,76 @@
--- Bootstrap Packer if not installed
-local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  print("Installing packer...")
-  vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.cmd 'packadd packer.nvim'
-  print("Packer installed!")
+-- Bootstrap Lazy.nvim if not installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  print("Installing lazy.nvim...")
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+  print("Lazy.nvim installed!")
 end
+vim.opt.rtp:prepend(lazypath)
 
--- Initialize and configure plugins
-return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim' -- Packer manages itself
-    use 'nvim-lua/plenary.nvim' -- Utility functions
+-- Plugin specifications
+require("lazy").setup({
+  -- Essential plugins
+  "nvim-lua/plenary.nvim", -- Utility functions (dependency for many plugins)
 
-    -- theme
-    use {
-        'catppuccin/nvim',
-        as = 'catppuccin', 
-        tags = 'v0.2.4.',
-        config = function()
-          require("catppuccin").setup({
-            transparent_background = false,
-        })
-        vim.cmd("colorscheme catppuccin-mocha")
-        end
-    }
-end)
+  -- Treesitter for syntax highlighting (load early)
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    priority = 100, -- Load early
+  },
+
+  -- Language Server Protocol support
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+        ensure_installed = { "lua_ls", "rust_analyzer", "codebook", "gopls", "ruby-lsp", "rubocop", "pyrefly" },
+    },
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
+  },
+
+  -- Autocompletion system
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
+      "hrsh7th/cmp-buffer",   -- Buffer source
+      "hrsh7th/cmp-path",     -- Path source
+      "L3MON4D3/LuaSnip",     -- Snippet engine
+      "saadparwaiz1/cmp_luasnip", -- Snippet source
+    },
+  },
+
+  -- File explorer
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+
+  -- Fuzzy finder
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
+
+  -- Key binding helper
+  {
+    "folke/which-key.nvim",
+  },
+
+  -- Theme (load last after all functionality is configured)
+  { 
+    "catppuccin/nvim", 
+    name = "catppuccin",
+    priority = 1000, -- Load last
+  },
+})
